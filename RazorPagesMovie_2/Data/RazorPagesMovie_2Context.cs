d76272fda510bc2b4b25d0336ea1924d346b51cc
nstratford76 <nstratford76@gmail.com>
2020-10-28 16:54:23 -0600

    Add project files.

---
+++ RazorPagesMovie_2/Data/RazorPagesMovie_2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie_2.Models;
+
+namespace RazorPagesMovie_2.Data
+{
+    public class RazorPagesMovie_2Context : DbContext
+    {
+        public RazorPagesMovie_2Context (DbContextOptions<RazorPagesMovie_2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie_2.Models.Movie> Movie { get; set; }
+    }
+}
